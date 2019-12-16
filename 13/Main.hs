244aa94d7a569056b3baefebcf467a1454f8c647
Andrew Halle <andrew@livelyme.com>
2019-12-15 18:26:02 -0800

    day 13 part 2

--- 13/Main.hs
+++ 13/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import Computer
+import System.IO
+import Data.List.Split
+
+data JoystickInput = LeftJoystick
+                   | NeutralJoystick
+                   | RightJoystick
+
+joystickInputAsInt :: JoystickInput -> Int
+joystickInputAsInt LeftJoystick = -1
+joystickInputAsInt NeutralJoystick = 0
+joystickInputAsInt RightJoystick = 1
 
 data TileType = EmptyTile
               | WallTile
@@ -18,6 +29,14 @@ isBlockTile :: Tile -> Bool
 isBlockTile (Tile _ _ BlockTile) = True
 isBlockTile (Tile _ _ _) = False
 
+isBallTile :: Tile -> Bool
+isBallTile (Tile _ _ BallTile) = True
+isBallTile (Tile _ _ _) = False
+
+isHorizontalPaddleTile :: Tile -> Bool
+isHorizontalPaddleTile (Tile _ _ HorizontalPaddleTile) = True
+isHorizontalPaddleTile (Tile _ _ _) = False
+
 tileTypeFromInt :: Int -> TileType
 tileTypeFromInt n = case n of
                       0 -> EmptyTile
@@ -26,6 +45,12 @@ tileTypeFromInt n = case n of
                       3 -> HorizontalPaddleTile
                       4 -> BallTile
 
+tileGetXCoordinate :: Tile -> Int
+tileGetXCoordinate (Tile x _ _) = x
+
+tileGetYCoordinate :: Tile -> Int
+tileGetYCoordinate (Tile _ y _) = y
+
 screenInsert :: Screen -> Int -> Int -> Int -> Screen
 screenInsert s x y t =
   let tileType = tileTypeFromInt t
@@ -33,27 +58,100 @@ screenInsert s x y t =
       rest = filter (not . tileAtPosition (x,y)) s
   in (tile:rest)
 
-screenApplyOutput :: Screen -> [Int] -> Screen
-screenApplyOutput start output =
-  case output of
-    []              -> start
-    (x1:x2:x3:rest) -> screenApplyOutput (screenInsert start x1 x2 x3) rest
 
-data ArcadeCabinet = ArcadeCabinet { screen :: Screen, computer :: VmState }
+data ArcadeCabinet = ArcadeCabinet { screen :: Screen, computer :: VmState, score :: Int }
 
 createArcadeCabinet :: String -> ArcadeCabinet
-createArcadeCabinet program = ArcadeCabinet { screen = [], computer = vmInitState program 4096 }
+createArcadeCabinet program = ArcadeCabinet { screen = [], computer = vmInitState program 4096, score = 0 }
 
-stepArcadeCabinet :: ArcadeCabinet -> ArcadeCabinet
-stepArcadeCabinet start@(ArcadeCabinet { screen=screen, computer=computer }) =
-  let (nextComputer, output) = vmRunUntilMoreInputRequired computer []
-      nextScreen = screenApplyOutput screen output
-  in ArcadeCabinet { computer=nextComputer, screen=nextScreen }
+arcadeCabinetApplyOutput :: ArcadeCabinet -> [Int] -> ArcadeCabinet
+arcadeCabinetApplyOutput start output =
+  case output of
+    []                -> start
+    (-1:0:score:rest) -> arcadeCabinetApplyOutput (start { score=score }) rest
+    (x1:x2:x3:rest) -> arcadeCabinetApplyOutput (start { screen=(screenInsert (screen start) x1 x2 x3) }) rest
+
+stepArcadeCabinet :: ArcadeCabinet -> JoystickInput -> ArcadeCabinet
+stepArcadeCabinet start@(ArcadeCabinet { screen=screen, computer=computer }) joystick =
+  let (nextComputer, output) = vmRunUntilMoreInputRequired computer [joystickInputAsInt joystick]
+      nextGame = arcadeCabinetApplyOutput start output
+  in nextGame { computer=nextComputer }
+
+arcadeWidth :: ArcadeCabinet -> Int
+arcadeWidth (ArcadeCabinet { screen=screen }) =
+  let xs = map tileGetXCoordinate screen
+  in maximum xs
+
+arcadeHeight :: ArcadeCabinet -> Int
+arcadeHeight (ArcadeCabinet { screen=screen }) =
+  let ys = map tileGetYCoordinate screen
+  in maximum ys
+
+arcadeOutputAsString :: ArcadeCabinet -> String
+arcadeOutputAsString a@(ArcadeCabinet { screen=screen, score=score }) =
+  let width = arcadeWidth a
+      height = arcadeHeight a
+      tiles = take (width*height) (foldr (buildString width screen) "" [0..])
+      ls = chunksOf width tiles
+      s = unlines ls
+  in s ++ ['\n'] ++ "Score: " ++ (show score)
+  where buildString width ts idx rest =
+          let x = idx `mod` width
+              y = idx `div` width
+              [(Tile _ _ t)] = filter (tileAtPosition (x,y)) ts
+              c = case t of
+                    EmptyTile -> ' '
+                    WallTile -> '|'
+                    BlockTile -> '#'
+                    HorizontalPaddleTile -> '='
+                    BallTile -> 'o'
+          in (c:rest)
+
+gameStillRunning :: ArcadeCabinet -> Bool
+gameStillRunning (ArcadeCabinet { computer=computer }) =
+  case computer of
+    Running _ _ _ -> True
+    _             -> False
+
+getBallPosition :: ArcadeCabinet -> (Int,Int)
+getBallPosition (ArcadeCabinet { screen=screen }) =
+  let ts = filter isBallTile screen
+      (Tile x y _) = case ts of
+                       [] -> Tile 0 0 EmptyTile
+                       [t] -> t
+  in (x,y)
+
+getPaddlePosition :: ArcadeCabinet -> (Int,Int)
+getPaddlePosition (ArcadeCabinet { screen=screen }) =
+  let ts = filter isHorizontalPaddleTile screen
+      (Tile x y _) = case ts of
+                       [] -> Tile 0 0 EmptyTile
+                       [t] -> t
+  in (x,y)
+
+runGame :: ArcadeCabinet -> IO ()
+runGame curr = do
+  -- j <- getChar
+  -- let joystickInput = case j of
+  --                       'a' -> LeftJoystick
+  --                       's' -> NeutralJoystick
+  --                       'd' -> RightJoystick
+  let (ballX,_) = getBallPosition curr
+      (paddleX,_) = getPaddlePosition curr
+      joystickInput = if paddleX == ballX
+                      then NeutralJoystick
+                      else if paddleX > ballX
+                           then LeftJoystick
+                           else RightJoystick
+      newGame = stepArcadeCabinet curr joystickInput
+  putStrLn (arcadeOutputAsString newGame)
+  if gameStillRunning newGame
+  then runGame newGame
+  else return ()
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
   prog <- readFile "program.txt"
   let game = createArcadeCabinet prog
-      (ArcadeCabinet { screen=screen }) = stepArcadeCabinet game
-      blockTiles = filter isBlockTile screen
-  print $ length blockTiles
+  runGame game
